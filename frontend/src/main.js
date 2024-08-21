758b78446ef41e7809efd2bbfe07f959208c5537
MAY35\Administrator <trong6so1@gmail.com>
2024-08-21 17:58:18 +0700

    fix

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,11 +4,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import { Menu, List, Drawer, Button, message } from 'ant-design-vue'
+import axios from 'axios'
 
 import 'ant-design-vue/dist/reset.css'
 import 'bootstrap/dist/css/bootstrap-grid.min.css'
 import 'bootstrap/dist/css/bootstrap-utilities.min.css'
 const app = createApp(App)
+
+window.axios = axios
 
 app.use(createPinia())
 app.use(router)
--- frontend/src/pages/admin/users/index.vue
+++ frontend/src/pages/admin/users/index.vue
@@ -3,10 +3,25 @@
 </template>
 <script>
 import { useMenu } from "@/stores/menu.js";
-
+import { ref } from "vue";
 export default {
   setup() {
     useMenu().onSelectedKeys(["users"]);
+    const users = ref([]);
+    users.value = this.getUsers();
+  },
+  methods: {
+    getUsers() {
+      axios
+        .get("http://localhost:8000/api/users/index")
+        .then((response) => {
+          console.log(response.data);
+          return 1;
+        })
+        .catch((error) => {
+          return error;
+        });
+    },
   },
 };
 </script>
